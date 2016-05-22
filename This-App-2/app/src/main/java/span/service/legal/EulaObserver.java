79b60574e11062cfecd639503f5bc8c253f48bfa
Odama77 <amadoitt@gmail.com>
2016-05-22 00:25:48 -0400

    Testing this project

---
+++ This-App-2/app/src/main/java/span/service/legal/EulaObserver.java
@@ -0,0 +1,13 @@
+/**
+ *  SPAN - Smart Phone Ad-Hoc Networking project
+ *  Copyright (c) 2012 The MITRE Corporation.
+ */
+package span.service.legal;
+
+public interface EulaObserver {
+
+	// callback methods
+	
+	public void onEulaAccepted();
+	
+}
